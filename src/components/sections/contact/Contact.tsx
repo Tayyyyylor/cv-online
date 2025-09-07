1bb479c57ded352553b97a8ac6c2db4902112832
taylor <bryan.h.pro@gmail.com>
2025-09-07 07:51:32 +0200

    add content on footer

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -2,16 +2,16 @@ import { ButtonContact } from "@/components/buttonContact/ButtonContact";
 import React from "react";
 import styles from "./Contact.module.scss";
 import classNames from "classnames/bind";
+import { useTranslations } from "next-intl";
 
 const cx = classNames.bind(styles);
 
 export const Contact = () => {
+  const t = useTranslations("Contact");
   return (
     <footer className={cx("contact")} id="contact">
       <h2 className={cx("contact__title", "heavitasFont")}>Contact</h2>
-      <p className={cx("contact__description")}>
-        Contactez moi par mail ou téléphone
-      </p>
+      <p className={cx("contact__description")}>{t("desc")}</p>
       <ButtonContact className={cx("contact__buttons")} />
     </footer>
   );
